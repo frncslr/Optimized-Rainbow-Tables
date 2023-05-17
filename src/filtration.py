a3c4b562de3de3dc16af32740d153218b102b425
francoisLEROUX <francois3443@gmail.com>
2023-05-17 14:12:30 +0200

    Python optimization script working

--- src/filtration.py
+++ src/filtration.py
@@ -1,5 +1,4 @@
 from scipy.optimize import minimize, NonlinearConstraint
-from math import floor
 
 N = 2**24
 t = 1000
@@ -16,7 +15,6 @@ vf = 1 # number of filtrations per second
 do = 0 # average overhead time per point
 dc = 0 # average nodes communication time
 
-# a = 31
 c0 = 0
 ca = t
 
@@ -24,110 +22,81 @@ ca = t
 def mci(i):
     return 2*N/(i+gamma)
 
-def precompH(pos):
+def precompH(filters):
     result = 0;
-    for i in range(1, len(pos), 1):
-        result += mci(i-1)*(pos[i]-pos[i-1])
+    for i in range(1, len(filters), 1):
+        result += mci(filters[i-1])*(filters[i]-filters[i-1])
     return result / (vh*nh)
 
-def precompF(pos):
+def precompF(filters):
     result = 0
-    for i in range(1, len(pos), 1):
-        result += mci(i-1)
+    for i in range(1, len(filters), 1):
+        result += mci(filters[i-1])
     return result / (vf*nf)
 
-def precompO(pos):
+def precompO(filters):
     result = 0
-    for i in range(1, len(pos), 1):
-        result += mci(i-1)
+    for i in range(1, len(filters), 1):
+        result += mci(filters[i-1])
     return do*result
 
-def precompC(pos):
+def precompC(filters):
     result = 0
-    for i in range(1, len(pos), 1):
-        result += mci(i-1)
+    for i in range(1, len(filters), 1):
+        result += mci(filters[i-1])
     return dc*result/nh 
 
-def precompT(pos):
-    return precompH(pos) + precompF(pos)
-
-def f(x):
-    return (x**2)-(12*x)+20
-
-def cons_inc(pos):
-    for i in range(1, len(pos), 1):
-        if not pos[i]-pos[i-1]:
-            return -1
-    return 0
-
-def cons_lim(pos):
-    result = 0
-    if pos[0] == c0:
-        result += 1
-    if pos[a] == ca:
-        result += 1
-    return result
-    # return 0 if (pos[0] == c0 and pos[a] == ca) else 1
-
-def ineq_cstr(pos):
-    for i in range(1,len(pos)):
-        if pos[i]-pos[i-1]-1 < 0:
-            return 1
-    return 0
+def precompT(filters):
+    return precompH(filters) + precompF(filters)
 
 def cstr_ineq(a):
     list_cstr = []
     for i in range(2,a+1):
-        def f(pos, i=i):
-            return pos[i]-pos[i-1]-1
+        def f(filters, i=i):
+            return filters[i]-filters[i-1]-1
         list_cstr.append(NonlinearConstraint(f, 0, 1000))
     return list_cstr
 
-def opti_filters(amax):
+def positions(amax):
     nb_filters = 0
     time = N*t
     filters = []
     for a in range(1, amax+1):
-        pos = [0]*(a+1)
+        x0 = [0]*(a+1)
         bnds = ((c0,c0),)+((1,ca),)*(a-1)+((ca,ca),)
-        list_cstr = cstr_ineq(a)
-        result = minimize(precompT, pos, method='trust-constr', bounds=bnds, constraints=list_cstr)
+        cstr = cstr_ineq(a)
+        result = minimize(precompT, x0, method='trust-constr', bounds=bnds, constraints=cstr)
         if result.fun < time:
             nb_filters = a
             time = result.fun
             filters = result.x
-    print(f"nb_filters : {nb_filters}")
-    filters[0] = 0
-    filters = list(map(lambda x: int(x), filters))
-    print(f"filters : {filters}")
-    print(f"time : {precompT(filters)}")
-    
+    print(f"T : {precompT(filters)}")
+    print(f"H : {precompH(filters)}")
+    print(f"F : {precompF(filters)}")
+    filters[0] = nb_filters
+    return list(map(lambda x: int(x), filters))
+ 
+def export(filters, filename):
+    with open(filename, "wb") as file:
+        for filter in filters:
+            file.write(filter.to_bytes(4, "big"))
+            print(filter.to_bytes(4, "big"))
+           
+def check(filename):
+    filters = []
+    with open(filename, "rb") as file:
+        nb_filters = int.from_bytes(file.read(4), "big")
+        content = file.read(nb_filters*4)
+    for i in range(nb_filters):
+        filters.append(int.from_bytes(content[4*i:4*(i+1)], "big"))
+    return nb_filters, filters
 
-if __name__ == "__main__":
-        
-    # pos = [i for i in range(a+1)]
-    # pos = [0]*(a+1)
-    # print(pos)
-    # bnds = ((c0,c0),)+((1,ca),)*(a-1)+((ca,ca),)
-    # print(bnds)
-    # # cons = ({'type':'eq', 'fun': lambda pos: pos[0]-c0},
-    # #         {'type':'eq', 'fun': lambda pos: pos[a]-ca},
-    # #         {'type':'ineq', 'fun': lambda pos : pos[1]-(floor(pos[0])+1)})
-    # # # cons = ({'type':'eq', 'fun': cons_lim})
-    # # result = minimize(precompT, pos, method='SLSQP', bounds=bnds, constraints=cons)
-    # # print(result)
-    # # print(result.x)
-    # # print(cons_inc(result.x))
-    # # print(cons_lim(result.x))
+if __name__ == "__main__": 
+    amax = 38
+    # filters = positions(amax)
+    # print(f"filters : {filters}")
+    # export(filters, "filters.dat")
     
-    #         # {'type':'eq','fun': lambda x : max([x[i]-int(x[i]) for i in range(len(x))])}
-    
-    # list_cstr = cstr_ineq()
-    # result = minimize(precompT, pos, method='trust-constr', bounds=bnds, constraints=list_cstr)
-    # print(result)
-    
-    
-    # print(ineq_cstr([0,1,1,1000]))
-    amax = 50
-    opti_filters(amax)
-    
+    # nb, f = check("filters.dat")
+    # print(nb)
+    # print(f)
