f429c188b86599a91ebdfd37cc7c1dfecd17cd38
francoisLEROUX <francois3443@gmail.com>
2023-05-30 12:12:06 +0200

    Plots update

--- src/fig.py
+++ src/fig.py
@@ -2,7 +2,7 @@ import numpy as np
 
 import matplotlib.pyplot as plt
 
-def draw_hOpe():
+def draw_operations():
     x = [0,1,2,3]
     y = [669750000]*4
     expected = 669750000
@@ -10,19 +10,19 @@ def draw_hOpe():
     ax.plot(x,y,'o',color='red',label='exemple plot')
     ax.axhline(y=expected, color='b', linestyle='--',label='exemple ligne')
     ax.set_title(f'Hash operations to precompute a table')
-    # plt.axis([0,4,66900000,670000000])
+    plt.axis([0,4,669000000,670000000])
     plt.xticks(np.arange(0, 4, 1), fontsize=16)
-    plt.yticks(np.arange(669000000, 670000000, 200000), fontsize=16)
-    # plt.yticks(fontsize=16)
+    plt.yticks(np.arange(669700000, 669800000, 10000), fontsize=16)
+    plt.yticks(fontsize=16)
 
     plt.xlabel("Table index ",fontsize=16)
     plt.ylabel("Hash operations",fontsize=16)
     plt.show()
     fig.savefig(f'name', bbox_inches='tight',dpi=500)
 
-draw_hOpe()
+# draw_operations()
 
-def draw_unikEP():
+def draw_endpoints():
     x = [0,1,2,3]
     y = [31921,31882,31965,31927]
     expected = 31893
@@ -37,8 +37,10 @@ def draw_unikEP():
     plt.ylabel("Unique endpoints",fontsize=16)
     plt.show()
     fig.savefig(f'name', bbox_inches='tight',dpi=500)
+    
+# draw_endpoints()
 
-def draw_cover():
+def draw_coverage():
     x = [0,1,2,3]
     y = [85.11,85.07,85.15,85.13]
     expected = 85.08
@@ -50,6 +52,8 @@ def draw_cover():
     plt.yticks(np.arange(84.7, 85.7, 0.2), fontsize=16)
 
     plt.xlabel("Table index ",fontsize=16)
-    plt.ylabel("Theorical coverage (%)",fontsize=16)
+    plt.ylabel("Theoretical coverage (%)",fontsize=16)
     plt.show()
     fig.savefig(f'name', bbox_inches='tight',dpi=500)
+    
+draw_coverage()
